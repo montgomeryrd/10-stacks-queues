c9e5ff3bdcd07f3704496f7acbf331a83cdfcc6f
montgomeryrd <montgomery_rd@yahoo.com>
2018-01-29 00:56:11 -0800

    stack.test.js complete

---
+++ lab-richard/__test__/queue.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Stack = require('../lib/stack');
+require('jest');
+
+describe('stack module', () => {
+    beforeEach(() => this.stack = new Stack());
+    describe('#push', () => {
+        it('should have a size of 10', () => {
+            
+        });
+    });
+});
--- lab-richard/__test__/stack.test.js
+++ lab-richard/__test__/stack.test.js
@@ -17,17 +17,28 @@ describe('stack module', () => {
         });
 
         it('should throw and error when the maxSize is met', () => {
-            expect(() => {
-                [...Array(10000)].map((e, i) => this.stack.push(~~(Math.random() * i)));
-            }).toThrow();
+            [...Array(1338)].map((e, i) => this.stack.push(~~(Math.random() * i)));
+            expect(this.stack.size).toEqual(1337);
         });
     });
 
     describe('#pop', () => {
-
+        it('should remove one element from a stack of 10 each time the user calls pop() on the stack', () => {
+            [...Array(10)].map((e, i) => this.stack.push(~~(Math.random() * i)));
+            this.stack.pop();
+            expect(this.stack.size).toEqual(9);
+            this.stack.pop();
+            expect(this.stack.size).toEqual(8);
+            this.stack.pop();
+            expect(this.stack.size).toEqual(7);
+        });
     });
 
     describe('#peek', () => {
+        it('should return what is on top of the stack', () => {
+            [...Array(10)].map((e, i) => this.stack.push(~~(Math.random() * i)));
+            expect(this.stack.peek()).toEqual(this.stack.top);
+        });
 
     });
 });
